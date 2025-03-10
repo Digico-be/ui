d657bbfa2e30bf614fe46611258f9e647fd54fec
Maxime Fondu <maximefondu@icloud.com>
2025-03-10 15:39:50 +0100

    Fix : fix import CustomSelect

--- src/components/Form/SelectCustom.tsx
+++ src/components/Form/SelectCustom.tsx
@@ -1,10 +1,5 @@
-import dynamic from 'next/dynamic'
-
 import { Controller, ControllerRenderProps, FieldValues, useFormContext } from 'react-hook-form'
-import clsx from 'clsx'
-import { Label } from './components/Label'
-
-const Select = dynamic(() => import('react-select'), { ssr: false })
+import { SimpleSelect } from './SimpleSelect'
 
 type OptionType = {
     label: string
@@ -20,54 +15,6 @@ type Props = {
     value?: string
     field?: ControllerRenderProps<FieldValues, string>
     onChange?: (value: string | number) => void
-}
-
-export const SimpleSelect = ({ label, onChange, className, field, ...props }: Props) => {
-    return (
-        <div className="flex flex-col gap-2">
-            <Label required={props.required}>{label}</Label>
-            <Select
-                {...field}
-                {...props}
-                //@ts-ignore
-                onChange={(selectedOption: any) => {
-                    field && field.onChange(selectedOption?.value)
-                    if (onChange) {
-                        onChange(selectedOption?.value)
-                    }
-                }}
-                value={props.options.find((opt) => opt.value === field.value) || null}
-                className={clsx('', className)}
-                styles={{
-                    control: (provided, state) => {
-                        return {
-                            ...provided,
-                            backgroundColor: state.isFocused ? '#f7f7f7' : 'transparent',
-                            borderColor: state.isFocused ? '#a5a5a5' : '#e5e5e5',
-                            borderWidth: '1px',
-                            borderRadius: '8px',
-                            fontSize: '16px',
-                            boxShadow: 'none',
-                            '&:hover': {
-                                borderColor: '#a5a5a5'
-                            }
-                        }
-                    },
-                    option: (provided, state) => ({
-                        ...provided,
-                        backgroundColor: state.isSelected ? '#2f6bff' : 'white',
-                        color: state.isSelected ? 'white' : '#111928',
-                        padding: '6px 8px',
-                        fontSize: '16px',
-                        cursor: 'pointer',
-                        '&:hover': {
-                            backgroundColor: '#EAF0FF'
-                        }
-                    })
-                }}
-            />
-        </div>
-    )
 }
 
 export const SelectReacHookForm = ({ className, name, label, onChange, ...props }: Props) => {
---
+++ src/components/Form/SimpleSelect.tsx
@@ -0,0 +1,71 @@
+import dynamic from 'next/dynamic'
+
+import { ControllerRenderProps, FieldValues } from 'react-hook-form'
+import clsx from 'clsx'
+import { Label } from './components/Label'
+
+const Select = dynamic(() => import('react-select'), { ssr: false })
+
+type OptionType = {
+    label: string
+    value: string | number
+}
+
+type Props = {
+    name?: string
+    required?: boolean
+    label?: string
+    className?: string
+    options: OptionType[]
+    value?: string
+    field?: ControllerRenderProps<FieldValues, string>
+    onChange?: (value: string | number) => void
+}
+
+export const SimpleSelect = ({ label, onChange, className, field, ...props }: Props) => {
+    return (
+        <div className="flex flex-col gap-2">
+            <Label required={props.required}>{label}</Label>
+            <Select
+                {...field}
+                {...props}
+                //@ts-ignore
+                onChange={(selectedOption: any) => {
+                    field && field.onChange(selectedOption?.value)
+                    if (onChange) {
+                        onChange(selectedOption?.value)
+                    }
+                }}
+                value={props.options.find((opt) => opt.value === field.value) || null}
+                className={clsx('', className)}
+                styles={{
+                    control: (provided, state) => {
+                        return {
+                            ...provided,
+                            backgroundColor: state.isFocused ? '#f7f7f7' : 'transparent',
+                            borderColor: state.isFocused ? '#a5a5a5' : '#e5e5e5',
+                            borderWidth: '1px',
+                            borderRadius: '8px',
+                            fontSize: '16px',
+                            boxShadow: 'none',
+                            '&:hover': {
+                                borderColor: '#a5a5a5'
+                            }
+                        }
+                    },
+                    option: (provided, state) => ({
+                        ...provided,
+                        backgroundColor: state.isSelected ? '#2f6bff' : 'white',
+                        color: state.isSelected ? 'white' : '#111928',
+                        padding: '6px 8px',
+                        fontSize: '16px',
+                        cursor: 'pointer',
+                        '&:hover': {
+                            backgroundColor: '#EAF0FF'
+                        }
+                    })
+                }}
+            />
+        </div>
+    )
+}
